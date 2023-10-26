e944200ba0ed7e82939917b3f4ba8e3246605cb2
“Keiori” <iwa.421112117.k9130@gmail.com>
2023-10-26 17:07:23 +0900

    Web page design1

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,134 +6,103 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <table class="table auto">
-                    <tr>
-                        <th>氏名</th>
-                        <td>{{ Auth::user()->family_name }} {{ Auth::user()->first_name }}</td>
-                    </tr>
-                    <tr>
-                        <th>性別</th>
-                        <td>{{ $sex_options[Auth::user()->sex]}}</td>
-                    </tr>
-                    <tr>
-                        <th>年齢</th>
-                        <td>{{ Auth::user()->age }} 歳</td>
-                    </tr>
-                    <tr>
-                        <th>所属</th>
-                        <td>{{ $institution_options[Auth::user()->institution] }}</td>
-                    </tr>
-                    <tr>
-                        <th>学年</th>
-                        <td>{{ $grade_options[Auth::user()->grade] }}</td>
-                    </tr>
-                    <tr>
-                        <th>指導可能教科</th>
-                        <td>
-                            @if (Auth::user()->subjects->isEmpty())
-                                未設定
-                            @else
-                                @foreach(Auth::user()->subjects as $subject)
-                                    {{ $subject->name }}
-                                @endforeach
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>指導歴</th>
-                        <td>
-                            @if(Auth::user()->teach_experience === null)
-                                未設定
-                            @else
-                                {{ $teach_experience_options[Auth::user()->teach_experience] }}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>実績</th>
-                        <td>
-                            @if(Auth::user()->record == null)
-                                未設定
-                            @else
-                                {{ Auth::user()->record }}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>自己紹介コメント</th>
-                        <td>
-                            @if(Auth::user()->comment == null)
-                                未設定
-                            @else
-                                {{ Auth::user()->comment }}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>プロフィール写真</th>
-                        <td>
-                            @if(Auth::user()->portrait_url == null)
-                                未設定
-                            @else
-                                {{ Auth::user()->portrait_url }}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>ZOOMリンク</th>
-                        <td>
-                            @if(Auth::user()->zoom_link == null)
-                                未設定
-                            @else
-                                {{ Auth::user()->zoom_link }}
-                            @endif
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <h2 class="font-semibold text-lg text-gray-800 leading-loose">今後の予定一覧</h2>
-                        @foreach ($future_events as $future_event)
-                            <div class="future_events_edit">
-                                <a href="/admin/dashboard/edit_schedule/{{ $future_event->id }}">
-                                    {{ $future_event->date->format('Y年n月j日') }}
-                                    {{ \Carbon\Carbon::parse($future_event->start_time)->format('H:i') }}
-                                        〜 {{ \Carbon\Carbon::parse($future_event->end_time)->format('H:i') }}
-                                    
-                                    @if($future_event->type == 0)
-                                        授業
-                                    @else
-                                        面談
-                                    @endif
-                                    
-                                    {{ $future_event->user->family_name }} {{ $future_event->user->first_name }}
-                                </a>
+        <div class"max-w-7xl mx-auto">
+            <div class="w-full h-screen grid grid-rows-3 grid-cols-4 gap-4">
+                <div class="row-start-1 row-end-4 col-start-1 col-end-3 bg-white border-4 border-cyan-800 p-6 m-3">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-loose">プロフィール</h2>
+                        <div class="h-1/2 grid grid-rows-5 grid-cols-4">
+                            <div class="col-span-4"></div>
+                            <div class="mx-4 rounded-full col-span-2 row-span-3 text-center bg-gray-300">Face Photo</div>
+                            <div class="mx-4 col-span-2 underline underline-offset-4 text-center">{{ Auth::user()->family_name }} {{ Auth::user()->first_name }}</div>
+                            <div class="mx-4 underline underline-offset-4 text-center">{{ $sex_options[Auth::user()->sex]}}</div>
+                            <div class="mx-4 underline underline-offset-4 text-center">{{ Auth::user()->age }} 歳</div>
+                            <div class="mx-4 underline underline-offset-4 text-center">{{ $institution_options[Auth::user()->institution] }}</div>
+                            <div class="mx-4 underline underline-offset-4 text-center">{{ $grade_options[Auth::user()->grade] }}</div>
+                            <div class="col-span-4"></div>
+                        </div>
+                    
+                    <div class="h-1/2 grid grid-rows-5">
+                        <div class="mx-4 underline underline-offset-4">
+                            指導可能教科 : 
+                                @if (Auth::user()->subjects->isEmpty())
+                                    未設定
+                                @else
+                                    @foreach(Auth::user()->subjects as $subject)
+                                        {{ $subject->name }}
+                                    @endforeach
+                                @endif
                             </div>
-                        @endforeach
-                    <div>
-                        <a href='/admin/dashboard/add_schedule'>新規予定登録</a>
+                        <div class="mx-4 underline underline-offset-4">
+                            指導歴 : 
+                                @if(Auth::user()->teach_experience === null)
+                                    未設定
+                                @else
+                                    {{ $teach_experience_options[Auth::user()->teach_experience] }}
+                                @endif
+                        </div>
+                        <div class="mx-4 underline underline-offset-4">
+                            実績 : 
+                                @if(Auth::user()->record == null)
+                                    未設定
+                                @else
+                                    {{ Auth::user()->record }}
+                                @endif
+                        </div>
+                        <div class="mx-4 underline underline-offset-4">
+                            ZOOMリンク : 
+                                @if(Auth::user()->zoom_link == null)
+                                    未設定
+                                @else
+                                    {{ Auth::user()->zoom_link }}
+                                @endif
+                        </div>
+                        <div class="mx-4 underline underline-offset-4">
+                            自己紹介コメント : 
+                                @if(Auth::user()->comment == null)
+                                    未設定
+                                @else
+                                    {{ Auth::user()->comment }}
+                                @endif
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
     
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <h2 class="font-semibold text-lg text-gray-800 leading-loose">過去の予定一覧</h2>
+    
+                <div class="row-start-1 row-end-3 col-start-3 col-end-5 border-4 bg-white border-cyan-800 p-6 m-3">
+                    <div class="grid grid-rows-3 grid-cols-2">
+                        <div class="row-start-1 row-end-2 col-start-1 col-end-2">
+                            <h2 class="font-semibold text-xl text-gray-800 leading-loose">今後の予定一覧</h2>
+                        </div>
+                        <div class="row-start-1 row-end-2 col-start-2 col-end-3 text-right">
+                            <a class="px-2 py-1 bg-blue-400 text-white font-semibold rounded hover:bg-indigo-500" href='/admin/dashboard/add_schedule'>新規予定登録</a>
+                        </div>
+                        <div class="row-start-2 row-end-4 col-start-1 col-end-3">
+                            @foreach ($future_events as $future_event)
+                                <div class="ml-4">
+                                    <a class="font-medium text-gray-700 hover:text-blue-700 underline underline-offset-2" href="/admin/dashboard/edit_schedule/{{ $future_event->id }}">
+                                        {{ $future_event->date->format('Y年n月j日') }}
+                                        {{ \Carbon\Carbon::parse($future_event->start_time)->format('H:i') }}
+                                            〜 {{ \Carbon\Carbon::parse($future_event->end_time)->format('H:i') }}
+                                        
+                                        @if($future_event->type == 0)
+                                            授業
+                                        @else
+                                            面談
+                                        @endif
+                                        
+                                        {{ $future_event->user->family_name }} {{ $future_event->user->first_name }}
+                                    </a>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="row-start-3 row-end-4 col-start-3 col-end-5 bg-white border-4 border-cyan-800 p-6 m-3">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-loose">過去の予定一覧</h2>
                         @foreach ($past_events as $past_event)
-                            <div class="past_events_edit">
-                                <a href="/admin/dashboard/edit_schedule/{{ $past_event->id }}">
+                            <div class="ml-4">
+                                <a class="font-medium text-gray-700 hover:text-blue-700 underline underline-offset-2" href="/admin/dashboard/edit_schedule/{{ $past_event->id }}">
                                     {{ $past_event->date->format('Y年n月j日') }}
                                     {{ \Carbon\Carbon::parse($past_event->start_time)->format('H:i') }}
                                         〜 {{ \Carbon\Carbon::parse($past_event->end_time)->format('H:i') }}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,95 +1,76 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('トップページ (生徒)') }}
-        </h2>
+        </h1>
     </x-slot>
     
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <table class="table auto">
-                    <tr>
-                        <th>氏名</th>
-                        <td>{{ Auth::user()->family_name }} {{ Auth::user()->first_name }}</td>
-                    </tr>
-                    <tr>
-                        <th>性別</th>
-                        <td>{{ $sex_options[Auth::user()->sex] }}</td>
-                    </tr>
-                    <tr>
-                        <th>学年</th>
-                        <td>{{ $grade_options[Auth::user()->grade] }}</td>
-                    </tr>
-                    <tr>
-                        <th>希望コマ数</th>
-                        <td>
-                            @if(Auth::user()->lesson_times == null)
-                                未設定
-                            @else
-                                {{ Auth::user()->lesson_times }}コマ
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>希望教科</th>
-                        <td>
-                            @if (Auth::user()->subjects->isEmpty())
-                                未設定
-                            @else
-                                @foreach(Auth::user()->subjects as $subject)
-                                    {{ $subject->name }}
-                                @endforeach
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>目標</th>
-                        <td>
-                            @if (Auth::user()->goal === null)
-                                未設定
-                            @else
-                                {{ $goal_options[Auth::user()->goal] }}
-                            @endif
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <h2 class="font-semibold text-lg text-gray-800 leading-loose">今後の予定一覧</h2>
-                        @foreach ($future_events as $future_event)
-                            <div class="future_events_show">
-                                <a href="/dashboard/show_schedule/{{ $future_event->id }}">
-                                    {{ $future_event->date->format('Y年n月j日') }}
-                                    {{ \Carbon\Carbon::parse($future_event->start_time)->format('H:i') }}
-                                        〜 {{ \Carbon\Carbon::parse($future_event->end_time)->format('H:i') }}
-                                    @if($future_event->type == 0)
-                                        授業
+        <div class="max-w-7xl mx-auto">
+            <div class="w-full h-screen grid grid-rows-3 grid-cols-4 gap-4">
+                <div class="row-start-1 row-end-4 col-start-1 col-end-3 bg-white border-4 border-cyan-700 p-6 m-3">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-loose">プロフィール</h2>
+                        <div class="h-3/4 grid grid-rows-7">
+                            <div class="row-start-1 row-end-2"></div>
+                            <div class="ml-4 row-start-2 row-end-3 underline underline-offset-4">氏名 : {{ Auth::user()->family_name }} {{ Auth::user()->first_name }}</div>
+                            <div class="ml-4 row-start-3 row-end-4 underline underline-offset-4">性別 : {{ $sex_options[Auth::user()->sex] }}</div>
+                            <div class="ml-4 row-start-4 row-end-5 underline underline-offset-4">学年 : {{ $grade_options[Auth::user()->grade] }}</div>
+                            <div class="ml-4 row-start-5 row-end-6 underline underline-offset-4">
+                                希望コマ数 : 
+                                    @if(Auth::user()->lesson_times == null)
+                                        未設定
                                     @else
-                                        面談
+                                        {{ Auth::user()->lesson_times }}コマ
                                     @endif
-                                </a>
                             </div>
-                        @endforeach
+                            <div class="ml-4 row-start-6 row-end-7 underline underline-offset-4">
+                                希望教科 : 
+                                    @if (Auth::user()->subjects->isEmpty())
+                                        未設定
+                                    @else
+                                        @foreach(Auth::user()->subjects as $subject)
+                                            {{ $subject->name }}
+                                        @endforeach
+                                    @endif
+                            </div>
+                            <div class="ml-4 row-start-7 row-end-8 underline underline-offset-4">
+                                目標 : 
+                                    @if (Auth::user()->goal === null)
+                                        未設定
+                                    @else
+                                        {{ $goal_options[Auth::user()->goal] }}
+                                    @endif
+                            </div>
+                        </div>
                 </div>
-            </div>
-        </div>
-    </div>
+
     
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <h2 class="font-semibold text-lg text-gray-800 leading-loose">過去の予定一覧</h2>
+                <div class="row-start-1 row-end-3 col-start-3 col-end-5 bg-white border-4 border-cyan-800 p-6 m-3">
+                    <div class="row-start-1 row-end-2 col-start-1 col-end-2">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-loose">今後の予定一覧</h2>
+                            @foreach ($future_events as $future_event)
+                                <div class="ml-4">
+                                    <a class="font-medium text-gray-700 hover:text-blue-700 underline underline-offset-2" href="/dashboard/show_schedule/{{ $future_event->id }}">
+                                        {{ $future_event->date->format('Y年n月j日') }}
+                                        {{ \Carbon\Carbon::parse($future_event->start_time)->format('H:i') }}
+                                            〜 {{ \Carbon\Carbon::parse($future_event->end_time)->format('H:i') }}
+                                        @if($future_event->type == 0)
+                                            授業
+                                        @else
+                                            面談
+                                        @endif
+                                    </a>
+                                </div>
+                            @endforeach
+                    </div>
+                </div>
+
+    
+                <div class="row-start-3 row-end-4 col-start-3 col-end-5 bg-white border-4 border-cyan-800 p-6 m-3">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-loose">過去の予定一覧</h2>
                         @foreach ($past_events as $past_event)
-                            <div class="past_events_show">
-                                <a href="/dashboard/show_schedule/{{ $past_event->id }}">
+                            <div class="ml-4">
+                                <a class="font-medium text-gray-700 hover:text-blue-700 underline underline-offset-2" href="/dashboard/show_schedule/{{ $past_event->id }}">
                                     {{ $past_event->date->format('Y年n月j日') }}
                                     {{ \Carbon\Carbon::parse($past_event->start_time)->format('H:i') }}
                                         〜 {{ \Carbon\Carbon::parse($past_event->end_time)->format('H:i') }}
